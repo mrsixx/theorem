cf20636a02e441a3f1a1d2f43345a80f5f1415f9
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2022-08-10 02:02:45 -0300

    Tests > Simple addition and multiplication with non-zero values

--- src/Theorem.Tests/UnitTest1.cs
+++ src/Theorem.Tests/UnitTest1.cs
@@ -6,9 +6,21 @@ namespace Theorem.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Class1_Soma_DeveRetornarOValorCorreto()
         {
+            var theorem = new Class1();
+            var resultado = theorem.Resolve("1+1");
 
+            Assert.Equal(2, resultado);
+        }
+
+        [Fact]
+        public void Class1_Multiplicacao_DeveRetornarOValorCorreto()
+        {
+            var theorem = new Class1();
+            var resultado = theorem.Resolve("5*3");
+
+            Assert.Equal(15, resultado);
         }
     }
 }
--- src/Theorem/Class1.cs
+++ src/Theorem/Class1.cs
@@ -4,5 +4,26 @@ namespace Theorem
 {
     public class Class1
     {
+        const char OPERADOR_ADICAO = '+';
+        const char OPERADOR_MULTIPLICACAO = '*';
+        public int Resolve(string exp)
+        {
+            if (exp.Contains(OPERADOR_ADICAO))
+            {
+                var termos = exp.Split(OPERADOR_ADICAO);
+                var operando1 = Convert.ToInt32(termos[0]);
+                var operando2 = Convert.ToInt32(termos[1]);
+                return operando1 + operando2;
+            }
+            else if (exp.Contains(OPERADOR_MULTIPLICACAO))
+            {
+                var termos = exp.Split(OPERADOR_MULTIPLICACAO);
+                var operando1 = Convert.ToInt32(termos[0]);
+                var operando2 = Convert.ToInt32(termos[1]);
+                return operando1 * operando2;
+            }
+
+            return 0;
+        }
     }
 }
